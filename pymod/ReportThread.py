b0f39dcd1e0f08bec23e2c89929ac5b753a7fdbe
Hrvoje Sute <sute.hrvoje@gmail.com>
2018-02-02 14:51:56 +0000

    write to avro file

--- pymod/ReportThread.py
+++ pymod/ReportThread.py
@@ -3,19 +3,12 @@ import threading, time
 from datetime import datetime, timedelta
 from ams_consumer.SharedSingleton import SharedSingleton
 
-class ReportThread:
-    _thread = None
+class ReportThread(threading.Thread):
 
     def __init__(self):
-        self._thread = threading.Thread(target=self.run, name='stat_report_thread')
-
-    def start(self):
-        self._thread.start()
+        threading.Thread.__init__(self, name='stat_report_thread')
 
     def run(self):
-        import ipdb
-  #      ipdb.set_trace()
-
         singleton = SharedSingleton()
         while True:
             hour24 = singleton.getLastStatTime() + timedelta(days = 1)
@@ -28,14 +21,14 @@ class ReportThread:
                 singleton.getLog().info(singleton.getLastStatTime().strftime('Since %Y-%m-%d %H:%M:%S messages consumed: %i') %
                      singleton.getMsgConsumed())
                 break
-            #ipdb.set_trace()
+
             if(singleton.getEventSigUsr1().isSet()):
                 singleton.getLog().info(singleton.getLastStatTime().strftime('Since %Y-%m-%d %H:%M:%S messages consumed: %i') %
                      singleton.getMsgConsumed())
                 singleton.resetCounters()
                 singleton.getEventSigUsr1().clear()
 
-            singleton.getLog().info('tredovaca ljuta')
+            #singleton.getLog().info('tredovaca ljuta')
             time.sleep(1)
 
 
