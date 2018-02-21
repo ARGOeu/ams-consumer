082aa9d87dcdffe99e6c9c1fccf096d49b7a6029
Daniel Vrcic <daniel.vrcic@gmail.com>
2018-02-21 11:40:37 +0100

    correct paths for argo-ams-consumer rename

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from distutils.core import setup
-import glob, sys
 
-NAME = 'ams-consumer'
+NAME = 'argo-ams-consumer'
 
 def get_ver():
     try:
@@ -10,18 +9,18 @@ def get_ver():
                 return line.split()[1]
     except IOError:
         print "Make sure that %s is in directory"  % (NAME+'.spec')
-        sys.exit(1)
+        raise SystemExit(1)
 
 setup(
     name=NAME,
     version=get_ver(),
     author='SRCE',
     author_email='dvrcic@srce.hr, hsute@srce.hr',
-    package_dir={'ams_consumer': 'pymod/'},
-    packages=['ams_consumer'],
+    package_dir={'argo_ams_consumer': 'pymod/'},
+    packages=['argo_ams_consumer'],
     url='https://github.com/ARGOeu/ams-consumer',
-    description='ams-consumer fetchs metric result messages from Argo Messaging System',
-    data_files=[('/etc/ams-consumer', ['config/ams-consumer.conf', 'config/metric_data.avsc']),
+    description='argo-ams-consumer fetchs metric result messages from Argo Messaging System',
+    data_files=[('/etc/argo-ams-consumer', ['config/ams-consumer.conf', 'config/metric_data.avsc']),
                 ('/usr/bin/', ['bin/ams-consumerd']),
                 ('/etc/init.d/', ['init/ams-consumer'])]
 )
