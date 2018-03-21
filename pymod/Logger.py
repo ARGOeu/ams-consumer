9806f69f05d54af43c2357aba6df459f326dd0cf
Daniel Vrcic <daniel.vrcic@gmail.com>
2018-03-21 11:32:35 +0100

    remove redudant caller name in logs

--- pymod/Logger.py
+++ pymod/Logger.py
@@ -12,7 +12,7 @@ class Logger(object):
     _logname = None
 
     def _init_stdout(self):
-        lfs = '%(levelname)s ' + self._caller + ' - %(message)s'
+        lfs = '%(levelname)s - %(message)s'
         lf = logging.Formatter(lfs)
         lv = logging.INFO
 
@@ -20,7 +20,7 @@ class Logger(object):
         self._logger = logging.getLogger(self._logname)
 
     def _init_syslog(self):
-        lfs = '%(name)s[%(process)s]: %(levelname)s ' + self._caller + ' - %(message)s'
+        lfs = '%(name)s[%(process)s]: %(levelname)s - %(message)s'
         lf = logging.Formatter(lfs)
         lv = logging.INFO
 
@@ -30,7 +30,7 @@ class Logger(object):
         self._logger.addHandler(sh)
 
     def _init_filelog(self, logfile):
-        lfs = '%(asctime)s %(name)s[%(process)s]: %(levelname)s ' + self._caller + ' - %(message)s'
+        lfs = '%(asctime)s %(name)s[%(process)s]: %(levelname)s  - %(message)s'
         lf = logging.Formatter(fmt=lfs, datefmt='%Y-%m-%d %H:%M:%S')
         lv = logging.INFO
 
@@ -40,8 +40,7 @@ class Logger(object):
         sf.setLevel(lv)
         self._logger.addHandler(sf)
 
-    def __init__(self, caller, logname, logdir):
-        self._caller = os.path.basename(caller)
+    def __init__(self, logname, logdir):
         self._logname = logname
         try:
             if not os.path.isdir(logdir):
@@ -50,7 +49,7 @@ class Logger(object):
             self._init_filelog(logdir + self._logname + '.log')
             self._init_syslog()
         except (OSError, IOError) as e:
-            sys.stderr.write('ERROR ' + self._caller + ' - ' + str(e) + '\n')
+            sys.stderr.write('ERROR - ' + str(e) + '\n')
             raise SystemExit(1)
 
     def get(self):
