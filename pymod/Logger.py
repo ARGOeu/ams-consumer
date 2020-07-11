32cb2a7c382a924bd25fe8ed604270fba301ee23
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-07-11 14:58:11 +0200

    remove daemonizing and syslog

--- pymod/Logger.py
+++ pymod/Logger.py
@@ -17,20 +17,8 @@ class Logger(object):
         lf = logging.Formatter(lfs)
         lv = logging.INFO
 
-        logging.basicConfig(format=lfs, level=lv, stream=sys.stdout)
+        logging.basicConfig(format=lfs, level=lv, stream=sys.stderr)
         self._logger = logging.getLogger(self._logname)
-
-    def _init_syslog(self):
-        lfs = '%(name)s[%(process)s]: %(levelname)s - %(message)s'
-        lf = logging.Formatter(lfs)
-        lv = logging.INFO
-
-        sh = logging.handlers.SysLogHandler('/dev/log', logging.handlers.SysLogHandler.LOG_USER)
-        sh.setFormatter(lf)
-        sh.setLevel(lv)
-        self._logger.addHandler(sh)
-        if self._logger_amslib:
-            self._logger_amslib.addHandler(sh)
 
     def _init_amslib(self):
         self._logger_amslib = logging.getLogger('argo_ams_library')
@@ -40,7 +28,7 @@ class Logger(object):
         lf = logging.Formatter(fmt=lfs, datefmt='%Y-%m-%d %H:%M:%S')
         lv = logging.INFO
 
-        sf = logging.handlers.RotatingFileHandler(logfile, maxBytes=512*1024, backupCount=4)
+        sf = logging.handlers.RotatingFileHandler(logfile, maxBytes=512 * 1024, backupCount=4)
         self._logger.fileloghandle = sf.stream
         sf.setFormatter(lf)
         sf.setLevel(lv)
@@ -57,7 +45,6 @@ class Logger(object):
             self._init_amslib()
             self._init_stdout()
             self._init_filelog(logdir + self._logname + '.log')
-            self._init_syslog()
         except (OSError, IOError) as e:
             sys.stderr.write('ERROR - ' + str(e) + '\n')
             raise SystemExit(1)
