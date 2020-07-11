5efc4aa97c92620948bd8514ce13ca926ee7eb52
Daniel Vrcic <daniel.vrcic@gmail.com>
2020-07-11 14:13:29 +0200

    build only Centos7/Python3 RPM

--- setup.py
+++ setup.py
@@ -3,33 +3,27 @@ import platform
 
 NAME = 'argo-ams-consumer'
 
-def is_c7():
-    dist = platform.dist()
-    for e in dist:
-        if e.startswith('7'):
-            return True
-    return False
 
 def get_ver():
     try:
-        for line in open(NAME+'.spec'):
+        for line in open(NAME + '.spec'):
             if "Version:" in line:
                 return line.split()[1]
     except IOError:
-        print "Make sure that %s is in directory"  % (NAME+'.spec')
+        print("Make sure that {} is in directory".format(NAME + '.spec'))
         raise SystemExit(1)
+
 
 setup(
     name=NAME,
     version=get_ver(),
     author='SRCE',
-    author_email='dvrcic@srce.hr, hsute@srce.hr',
+    author_email='dvrcic@srce.hr',
     package_dir={'argo_ams_consumer': 'pymod/'},
     packages=['argo_ams_consumer'],
     url='https://github.com/ARGOeu/ams-consumer',
     description='argo-ams-consumer fetchs metric result messages from Argo Messaging System',
     data_files=[('/etc/argo-ams-consumer', ['config/ams-consumer.conf', 'config/metric_data.avsc']),
                 ('/usr/bin/', ['bin/ams-consumerd']),
-                ('/usr/lib/systemd/system/', ['init/ams-consumer.service']) if is_c7() else \
-                ('/etc/init.d/', ['init/ams-consumer'])]
+                ('/usr/lib/systemd/system/', ['init/ams-consumer.service'])]
 )
